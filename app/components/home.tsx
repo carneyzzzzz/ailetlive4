d7030298dddc4554b489dc34607990c0eab7c6da
carneyzzzzz <125590231+carneyzzzzz@users.noreply.github.com>
2023-04-26 18:32:50 +0800

    Update home.tsx

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -25,6 +25,8 @@ import dynamic from "next/dynamic";
 import { REPO_URL } from "../constant";
 import { ErrorBoundary } from "./error";
 
+import { text as SensitiveText } form '../sensitive-text';
+
 export function Loading(props: { noLogo?: boolean }) {
   return (
     <div className={styles["loading-content"]}>
